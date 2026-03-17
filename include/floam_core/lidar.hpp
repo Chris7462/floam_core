bee7663d6c3379f4358f7c2585cd99cf927c57c8
chris7462 <chris7462@gmail.com>
2026-03-17 12:33:43 -0400

    update naming conversion

--- include/floam_core/lidar.hpp
+++ include/floam_core/lidar.hpp
@@ -8,12 +8,12 @@ class Lidar
 public:
   Lidar() = default;
 
-  void setScanPeriod(double scan_period_in);
-  void setLines(int num_lines_in);
-  void setVerticalAngle(double vertical_angle_in);
-  void setVerticalResolution(double vertical_angle_resolution_in);
-  void setMaxDistance(double max_distance_in);
-  void setMinDistance(double min_distance_in);
+  void set_scan_period(double scan_period_in);
+  void set_lines(int num_lines_in);
+  void set_vertical_angle(double vertical_angle_in);
+  void set_vertical_resolution(double vertical_angle_resolution_in);
+  void set_max_distance(double max_distance_in);
+  void set_min_distance(double min_distance_in);
 
   double max_distance;
   double min_distance;
--- src/lidar.cpp
+++ src/lidar.cpp
@@ -5,32 +5,32 @@
 namespace floam_core
 {
 
-void Lidar::setLines(int num_lines_in)
+void Lidar::set_lines(int num_lines_in)
 {
   num_lines = num_lines_in;
 }
 
-void Lidar::setVerticalAngle(double vertical_angle_in)
+void Lidar::set_vertical_angle(double vertical_angle_in)
 {
   vertical_angle = vertical_angle_in;
 }
 
-void Lidar::setVerticalResolution(double vertical_angle_resolution_in)
+void Lidar::set_vertical_resolution(double vertical_angle_resolution_in)
 {
   vertical_angle_resolution = vertical_angle_resolution_in;
 }
 
-void Lidar::setScanPeriod(double scan_period_in)
+void Lidar::set_scan_period(double scan_period_in)
 {
   scan_period = scan_period_in;
 }
 
-void Lidar::setMaxDistance(double max_distance_in)
+void Lidar::set_max_distance(double max_distance_in)
 {
   max_distance = max_distance_in;
 }
 
-void Lidar::setMinDistance(double min_distance_in)
+void Lidar::set_min_distance(double min_distance_in)
 {
   min_distance = min_distance_in;
 }
