34faf5f73362678386228e40027654ad285cd809
chris7462 <chris7462@gmail.com>
2026-03-17 12:52:30 -0400

    update Lidar struct

--- include/floam_core/lidar.hpp
+++ include/floam_core/lidar.hpp
@@ -3,18 +3,8 @@
 namespace floam_core
 {
 
-class Lidar
+struct Lidar
 {
-public:
-  Lidar() = default;
-
-  void set_scan_period(double scan_period_in);
-  void set_lines(int num_lines_in);
-  void set_vertical_angle(double vertical_angle_in);
-  void set_vertical_resolution(double vertical_angle_resolution_in);
-  void set_max_distance(double max_distance_in);
-  void set_min_distance(double min_distance_in);
-
   double max_distance;
   double min_distance;
   int num_lines;
